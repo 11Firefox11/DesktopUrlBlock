0f920a2affe6953a67d474bf6c6cbe6a701d4128
11Firefox11 <t.viktorvagyok@gmail.com>
2021-09-21 21:44:49 +0200

    Plan removed, copied it to a table. Little work

--- Plan.md
+++ Plan.md
@@ -7,17 +7,3 @@ So the user could define, what websites/urls should be blocked, and how.
 There would be presets in a json file, you could give presets a name, edit them, or you can just add some urls no matter to the presets.  
 You could add timers, to what should be executed when.  
 There would be a special web server, what would be an "extension" for the app, it could do some simple things, like by the redirecting url, count how many times the user tried to go on a curtain website and when, but for now, let's just leave that for a future plan.  
-## Main classes
-### Main 
-ClassVariables: Version, DefEpilog, OutputTypes 
-Methods: InitArgpharse, add, remove, preset(load, add, remove, reset, set), reset
-StaticMethods: Output
-### HostFile
-ClassVariables: DefaultTopText, DefaultRedirectUrl
-Methods: Add, Remove, SetContent
-StaticMethods: MakeSyntax
-Properties: CurrentContent, Path  
-### ConfigFile
-ClassVariables: Path
-Methods: Load, Add, Remove, Reset, Set
-### UrlBlockExceptions
--- modules/ManageHostFile.py
+++ modules/ManageHostFile.py
@@ -1,42 +1,39 @@
-import sys
+import sys, ctypes
 from UrlBlockExceptions import *
 class HostFile:
 
     DefaultRedirectUrl = "127.0.0.1"
-    DefaultTopText = "# This host file was edited with DesktopUrlBlock.\n# For more information and help, go to the app's github page: https://github.com/11Firefox11/DesktopUrlBlock."
+    DefaultTopText = "# This host file was edited with DesktopUrlBlock.\n# For more information and help, go to the app's github page: https://github.com/11Firefox11/DesktopUrlBlock.\n"
     PlatformsPath = {"win":"c:\windows\system32\drivers\etc\hosts", "linux":"/etc/hosts", "darwin":"/etc/hosts"}
 
-    @staticmethod
-    def SetContent(content):
-        if HostFile.CheckForPermission() and content != None and content != "":
+    def SetContent(self, content, settype=None):
+        if self.CheckForPermission() and content != None and content != "":
             if type(content) == list:
                 finalcontent = ""
                 for part in content:
                     if part != "" and part != None:
                         finalcontent += str(part) + "\n"
                 content = finalcontent
+            if settype == "add":
+                self.HostFileContent
             with open(HostFilePath, "w+") as file:
-                file.write(f"{HostFile.DefaultTopText}\n{content}")
+                file.write(f"{HostFile.DefaultTopText}{content}")
             return True
 
-    @staticmethod
-    def Remove(o):
-        pass
-
-    @staticmethod
-    def Add(o):
-        pass
-
     @property
-    def HostFileContent():
+    def HostFileContent(self):
         if HostFile.CheckForPermission():
-            return open(HostFilePath, "r+").readlines()
+            text = open(HostFilePath, "r").readlines()
+            if len(text) > 2 and "".join(text[:2]) == HostFile.DefaultTopText:
+                return text[2:]
+            else:
+                print("".join(text[:2]), HostFile.DefaultTopText)
+                return text
 
     @staticmethod
     def CheckForPermission():
         try:
-            with open(HostFilePath, "w+") as file:
-                pass
+            open(HostFilePath, "a")
             return True
         except:
             raise PermissionNotGranted(HostFilePath)
@@ -51,4 +48,5 @@ class HostFile:
 
 HostFilePath = "".join([HostFile.PlatformsPath[pl] for pl in HostFile.PlatformsPath if sys.platform.startswith(pl)])
 
-print(HostFile().SetContent(["xd",""]))
+HostFile().SetContent("xd")
+print(HostFile().HostFileContent)
